33745e9f8d752499fe3fd5ba5f5427f33f34b4af
Moon Antonio <pinchodev91@gmail.com>
2018-05-09 18:01:12 +0200

    Plugin

---
+++ LvlAvanzado Project/Assets/LvlA/Scripts/PluginHelp.cs
@@ -0,0 +1,26 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// PluginHelp.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace LvlA
+{
+	public class PluginHelp : MonoBehaviour
+	{
+		public UDebug udebug;
+
+		private void Start()
+		{
+			udebug = new UDebug();
+			udebug.UDebugLog("Metodo de la DLL.");
+		}
+	}
+}
